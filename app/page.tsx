c7f36de971986accae5273dffdd03b3d9768579a
sandervaneck <69575096+sandervaneck@users.noreply.github.com>
2024-02-14 13:40:24 +0100

    Fix textual issues with deployment

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Sander van Eck's portfolio&nbsp;
+          Sander van Eck portfolio&nbsp;
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
@@ -26,7 +26,7 @@ export default function Home() {
           </a>
         </div>
       </div>
-      Commercial and professional use cases:
+      <>Commercial and professional use cases:</>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://www.mychefsbase.com"
@@ -71,7 +71,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Private label Yerba Maté products, here is the self-developed webshop. 
+            Private label Yerba Mate products, here is the self-developed webshop. 
           </p>
           <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
@@ -110,7 +110,7 @@ export default function Home() {
         </a>
         </div>
 
-        Public use cases:
+        <>Public use cases:</>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left"> 
         <a
